922424a80d67f631a3fe298b4e04819167d57d0a
EDEN-NN <walivitorio@gmail.com>
2021-12-09 18:26:40 -0300

    Payment, one-to-one association

--- src/main/java/com/walison/course/config/TestConfig.java
+++ src/main/java/com/walison/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.walison.course.entities.Category;
 import com.walison.course.entities.Order;
 import com.walison.course.entities.OrderItem;
+import com.walison.course.entities.Payment;
 import com.walison.course.entities.Product;
 import com.walison.course.entities.User;
 import com.walison.course.entities.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-21T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
